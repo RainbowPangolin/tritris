dfdefd52b233c2ba5c56d33f03795b14e2618092
Kaiwen Tsou <kktsou@ucsd.edu>
2023-02-20 12:19:13 -0800

    added a single comment :P

--- game/activePiece.js
+++ game/activePiece.js
@@ -1,5 +1,5 @@
 //TODO: Attach a ShadowPiece as an Observer?
 
 class ActivePiece extends Piece{
-
+    //whenever activepiece moves, shadow piece moves to where active piece is, inherits rotation, and harddrops itself
 }
