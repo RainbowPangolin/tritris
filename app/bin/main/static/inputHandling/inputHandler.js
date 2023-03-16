9a66e8d689686a9e62d46e17d58fe4215fab2c6e
Kaiwen Tsou <kktsou@ucsd.edu>
2023-03-15 22:12:39 -0700

    Inputhandling framework

---
+++ app/bin/main/static/inputHandling/inputHandler.js
@@ -0,0 +1,75 @@
+// Define a default configuration object
+const defaultConfig = {
+    bindings: {
+      'k': 'MOVEDOWN',
+      'l': 'MOVERIGHT',
+      'j': 'MOVELEFT',
+      'a': 'ROTATELEFT',
+      'd': 'ROTATERIGHT',
+      'w': 'ROTATE180',
+      'i': 'HOLD',
+      ' ': 'HARDDROP',
+      't': 'START',
+      'y': 'END',
+      'r': 'RESTART'
+    },
+    turboDelay: 200, // milliseconds before the first repeat
+    turboInterval: 20 // milliseconds between repeats
+}
+
+class InputHandler{
+    constructor(playerBoard){
+        this.playerBoard = playerBoard
+        this.config = JSON.parse(localStorage.getItem('config')) || defaultConfig
+
+        this.lastTime = 0
+        this.turboRepeat = null
+        this.turboDelay = 200 // default turbo delay in ms
+        this.turboInterval = 20 
+    }
+
+    handleKeyDown(event) {
+        const input = config.bindings[event.key]
+        if (input) {
+            this.playerBoard.receiveInput(input)
+            event.preventDefault()
+    
+            // Handle turbo mode
+            if (event.repeat && this.turboRepeat === null) {
+                // Start turbo mode
+                this.turboRepeat = requestAnimationFrame(this.turboInput.bind(null, input))
+            } else if (!event.repeat && this.turboRepeat !== null) {
+                // End turbo mode
+                cancelAnimationFrame(this.turboRepeat)
+                this.turboRepeat = null
+            }
+        }
+    }
+
+    turboInput(input, timestamp) {
+        if (timestamp - this.lastTime >= this.turboInterval) {
+            this.playerBoard.receiveInput(input)
+            this.lastTime = timestamp
+        }
+        this.turboRepeat = requestAnimationFrame(turboInput.bind(null, input))
+    }
+
+    bindHandlerToDocument(domDocument){
+        domDocument.addEventListener("keydown", this.handleKeyDown)
+    }
+
+    updateBindings(bindings) {
+        this.config.bindings = bindings
+        localStorage.setItem('config', JSON.stringify(config))
+    }
+
+    updateTurboDelay(delay) {
+        this.config.turboDelay = delay
+        localStorage.setItem('config', JSON.stringify(config))
+    }
+
+    updateTurboInterval(interval) {
+        this.config.turboInterval = interval
+        localStorage.setItem('config', JSON.stringify(config))
+    }
+}
--- app/bin/main/static/main.js
+++ app/bin/main/static/main.js
@@ -1,5 +1,6 @@
 import clientPlayer from './game/ClientPlayer/CreateClientPlayer.js'
 import connectedPlayer from './game/ExtraPlayers/CreateSecondPlayer.js'
+import {InputHandler} from './inputHandling/InputHandler.js'
 import {ping} from './net/Pinger.js'
 
 let playerBoard = clientPlayer;
@@ -23,42 +24,46 @@ document.body.appendChild(pingATestButton)
 document.body.appendChild(pingBTestButton)
 
 
-document.addEventListener("keydown", (event) => {
-    switch(event.key){
-        case 'k':
-            playerBoard.receiveInput('MOVEDOWN')
-            break
-        case 'l':
-            playerBoard.receiveInput('MOVERIGHT')
-            break
-        case 'j':
-            playerBoard.receiveInput('MOVELEFT')
-            break        
-        case 'a':
-            playerBoard.receiveInput('ROTATELEFT')
-            break
-        case 'd':
-            playerBoard.receiveInput('ROTATERIGHT')
-            break
-        case 'w':
-            playerBoard.receiveInput('ROTATE180')
-            break
-        case 'i':
-            playerBoard.receiveInput('HOLD')
-            break
-        case ' ':
-            playerBoard.receiveInput('HARDDROP')
-            break
-        case 't':
-            playerBoard.receiveInput('START')
-            break   
-        case 'y':
-            playerBoard.receiveInput('END')
-            break 
-        case 'r':
-            playerBoard.receiveInput('RESTART')
-            break                   
+let clientInputHandler = new InputHandler(playerBoard);
+clientInputHandler.bindHandlerToDocument(document);
 
-    }
+
+// document.addEventListener("keydown", (event) => {
+//     switch(event.key){
+//         case 'k':
+//             playerBoard.receiveInput('MOVEDOWN')
+//             break
+//         case 'l':
+//             playerBoard.receiveInput('MOVERIGHT')
+//             break
+//         case 'j':
+//             playerBoard.receiveInput('MOVELEFT')
+//             break        
+//         case 'a':
+//             playerBoard.receiveInput('ROTATELEFT')
+//             break
+//         case 'd':
+//             playerBoard.receiveInput('ROTATERIGHT')
+//             break
+//         case 'w':
+//             playerBoard.receiveInput('ROTATE180')
+//             break
+//         case 'i':
+//             playerBoard.receiveInput('HOLD')
+//             break
+//         case ' ':
+//             playerBoard.receiveInput('HARDDROP')
+//             break
+//         case 't':
+//             playerBoard.receiveInput('START')
+//             break   
+//         case 'y':
+//             playerBoard.receiveInput('END')
+//             break 
+//         case 'r':
+//             playerBoard.receiveInput('RESTART')
+//             break                   
+
+//     }
     
-})
+// })
