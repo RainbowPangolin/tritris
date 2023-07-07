93cdbbcb51da8925aae6f8a450ea7f1942bd28aa
Kaiwen Tsou <kktsou@ucsd.edu>
2023-07-06 21:38:07 -0700

    Removed logs

--- app/bin/main/static/main.js
+++ app/bin/main/static/main.js
@@ -3,9 +3,6 @@ import connectedPlayer from './game/ExtraPlayers/CreateSecondPlayer.js'
 import {InputHandler} from './inputHandling/InputHandler.js'
 
 document.getElementById("startsingleplayer").addEventListener("click", function() {
-    // Code to execute when the button is clicked
-    console.log("Button clicked!");
-
     let playerBoard = clientPlayer;
 
     let clientInputHandler = new InputHandler(playerBoard);
@@ -19,9 +16,6 @@ document.getElementById("startsingleplayer").addEventListener("click", function(
 
 
 document.getElementById("startmultiplayer").addEventListener("click", function() {
-    // Code to execute when the button is clicked
-    console.log("Button clicked!");
-
     let playerBoard = clientPlayer;
 
     let clientInputHandler = new InputHandler(playerBoard);
